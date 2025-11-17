cd9da48d7ca3f72b4b44decfa445afb37a410b93
Ankitkumar-dewangan <ankitdewangan1122@gmail.com>
2025-11-17 15:34:34 +0530

    complete new

--- src/Components/Interface/Contact/Contact.js
+++ src/Components/Interface/Contact/Contact.js
@@ -55,7 +55,7 @@ const Contact = ({ animateKey }) => {
   const onFinish = async (values) => {
     setIsSubmitting(true);
     try {
-      const response = await fetch("https://newportfoliobackend-5mi4.onrender.com", {
+      const response = await fetch("https://newportfoliobackend-5mi4.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
